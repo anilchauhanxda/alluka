34923ca42ea715372dac6da2e37ef5f034019e4e
Anil Chauhan <60502277+anilchauhanxda@users.noreply.github.com>
2020-03-13 07:09:01 +0000

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -145,6 +145,9 @@ class Config(object):
     DEFAULT_NAME = os.environ.get("DEFAULT_NAME", None)
     #MongoDB
     MONGO_URI = os.environ.get("MONGO_URI", None)
+    
+    #walatermark dir
+    watermark_path = os.environ.get("watermark_path", None)
     # define the "types" that should be uplaoded as streamable
     TL_VID_STREAM_TYPES = ("MP4", "WEBM")
     TL_MUS_STREAM_TYPES = ("MP3", "WAV", "FLAC", "M4A")
