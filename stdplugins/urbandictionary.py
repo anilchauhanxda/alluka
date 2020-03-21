e46b8ca8699b2769bde82ac6b50a66934b37b663
Anil Chauhan <60502277+anilchauhanxda@users.noreply.github.com>
2020-03-21 20:18:26 +0000

    Update and rename urbandictionary.pi to urbandictionary.py

--- stdplugins/urbandictionary.py
+++ stdplugins/urbandictionary.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """Urban Dictionary
-Syntax: .ud Query"""
+Syntax: *ud Query"""
 from telethon import events
 import urbandict
 from uniborg.util import admin_cmd
