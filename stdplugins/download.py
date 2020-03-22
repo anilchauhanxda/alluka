a4f7fbf7cffc31e0093459ea99749097d4e71dd6
Anil Chauhan <60502277+anilchauhanxda@users.noreply.github.com>
2020-03-22 12:36:41 +0000

    Update download.py

--- stdplugins/download.py
+++ stdplugins/download.py
@@ -1,7 +1,7 @@
 """Download Files to your local server
 Syntax:
-*download
-*download url | file.name to download files from a Public Link
+.download
+.download url | file.name to download files from a Public Link
 customized by @meanii 
 Please Don't remove credit name 
 """
