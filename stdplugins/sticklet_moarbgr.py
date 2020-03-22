a2787d65fd03c71e66f7f80346ef6fd39cf07dd4
Anil Chauhan <60502277+anilchauhanxda@users.noreply.github.com>
2020-03-22 12:33:07 +0000

    Update sticklet_moarbgr.py

--- stdplugins/sticklet_moarbgr.py
+++ stdplugins/sticklet_moarbgr.py
@@ -1,65 +1,38 @@
 import io
-import textwrap
+import os
 import random
+import textwrap
 from telethon import events
-
 from PIL import Image, ImageDraw, ImageFont
-
+from telethon.tl.types import InputMessagesFilterDocument
 from uniborg.util import admin_cmd
 
+
 @borg.on(admin_cmd(pattern="srgb (.*)"))
-@borg.on(events.NewMessage(pattern=r"\*srgb ?(.*)",incoming=True))
+@borg.on(events.NewMessage(pattern=r"\.srgb ?(.*)",incoming=True))
 async def sticklet(event):
-    
     R = random.randint(0,256)
     G = random.randint(0,256)
     B = random.randint(0,256)
-    FC = random.randint(1,16)
+
+    # get the input text
+    # the text on which we would like to do the magic on
     sticktext = event.pattern_match.group(1)
 
-    if not sticktext:
-        await event.edit("`I need text to sticklet!`")
-        return
-
+    # delete the userbot command,
+    # i don't know why this is required
     await event.delete()
 
+    # https://docs.python.org/3/library/textwrap.html#textwrap.wrap
     sticktext = textwrap.wrap(sticktext, width=10)
+    # converts back the list to a string
     sticktext = '\n'.join(sticktext)
 
     image = Image.new("RGBA", (512, 512), (255, 255, 255, 0))
     draw = ImageDraw.Draw(image)
     fontsize = 230
 
-    if FC==1:
-      FONT_FILE = "Fonts/ActionNowPersonalUseRegular-nRvGV.ttf"
-    if FC==2:
-      FONT_FILE = "Fonts/AlouettePersonalUse-PKG4P.ttf"
-    if FC==3:
-      FONT_FILE = "Fonts/CakeNom-87Y0.ttf"
-    if FC==4:
-      FONT_FILE = "Fonts/Candyinc-9Gl2.otf"
-    if FC==5:
-      FONT_FILE = "Fonts/ConfettiStream-A6o6.ttf"
-    if FC==6:
-      FONT_FILE = "Fonts/KgHappyShadows-7KqA.ttf"
-    if FC==7:
-      FONT_FILE = "Fonts/Lsmiserableandmagical-3jDy.otf"
-    if FC==8:
-      FONT_FILE = "Fonts/MarvelousScriptDemo-7lGV.otf"
-    if FC==9:
-      FONT_FILE = "Fonts/MyFontAddictionRegular-nEWV.ttf"
-    if FC==10:
-      FONT_FILE = "Fonts/NewfontRegular-8An0.ttf"
-    if FC==11:
-      FONT_FILE = "Fonts/Painter-LxXg.ttf"
-    if FC==12:
-      FONT_FILE = "Fonts/PaperSign-JRRda.ttf"
-    if FC==13:
-      FONT_FILE = "Fonts/Ramadhankarim-K6WD.otf"
-    if FC==14:   
-      FONT_FILE = "Fonts/SweetNovember-n6O1.ttf"
-    if FC==15:
-      FONT_FILE = "Fonts/Variety-vjZ4.ttf"
+    FONT_FILE = await get_font_file(event.client, "@FontRes")
 
     font = ImageFont.truetype(FONT_FILE, size=fontsize)
 
@@ -71,9 +44,31 @@ async def sticklet(event):
     draw.multiline_text(((512-width)/2,(512-height)/2), sticktext, font=font, fill=(R, G, B))
 
     image_stream = io.BytesIO()
-    image_stream.name = "alluka.webp"
+    image_stream.name = "@allukabot.webp"
     image.save(image_stream, "WebP")
     image_stream.seek(0)
 
-    await event.client.send_file(event.chat_id, image_stream, reply_to=event.message.reply_to_msg_id)
-    await event.delete()
+    # finally, reply the sticker
+    await event.reply("allukabot", file=image_stream)
+
+    # cleanup
+    try:
+        os.remove(FONT_FILE)
+    except:
+        pass
+
+
+async def get_font_file(client, channel_id):
+    # first get the font messages
+    font_file_message_s = await client.get_messages(
+        entity=channel_id,
+        filter=InputMessagesFilterDocument,
+        # this might cause FLOOD WAIT,
+        # if used too many times
+        limit=None
+    )
+    # get a random font from the list of fonts
+    # https://docs.python.org/3/library/random.html#random.choice
+    font_file_message = random.choice(font_file_message_s)
+    # download and return the file path
+    return await client.download_media(font_file_message)
