3545d90fae9bca14491a9617e736d1ce4c951297
Anil Chauhan <60502277+anilchauhanxda@users.noreply.github.com>
2020-03-22 12:37:22 +0000

    Update eval.py

--- stdplugins/eval.py
+++ stdplugins/eval.py
@@ -1,5 +1,5 @@
 """Evaluate Python Code inside Telegram
-Syntax: `*eval {PythonCode}`
+Syntax: `.eval {PythonCode}`
 """
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
