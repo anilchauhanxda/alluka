71147a83649c75e202c3d379f22a865adbfa3e26
Anil Chauhan <60502277+anilchauhanxda@users.noreply.github.com>
2020-03-26 07:28:55 +0000

    Update logs.py

--- stdplugins/logs.py
+++ stdplugins/logs.py
@@ -15,6 +15,14 @@ async def _(event):
         return
     mentions = """**αℓℓυкα Zᴏʟᴅʏᴄᴋ™** //logs\n\n//8th feb 2020//\n• Fix `.kang` double reply.\n• Added new plugin `. into ur count` To view my stats 😉 **FOR SUDO USER ONLY**\n\n//10th feb 2020//\n• Added `.ai` (Your message) AI chat Bot 😉 [BUT VERY SLOW TO REPLY 😕]\n\n//11th Feb 2020//\n• Added `.slap` in reply to any message, or u gonna slap urself.\n• Added `.rnupload` file.name\n\n//12th feb 2020// \n• Added `.ft` (any emoji) 
     \n//13 March 2020//\n• Change prefix .ud to .mean \n• Added `.rrgb` Random RGB text Sticker\n• Added `.tagall` to tag all ppl in chat \n• Added `.commit` to upload plugins into ur github ripo (SUDO ONLY)
+    
+    //26 March 2020//
+    •Added `.decide` to get ans YES or NO
+    •Added `.paste`paste bin 
+    •Added `.userlist` to get all users in your chat.
+    •Added `.setwelcome` set welcome message in your chat.
+    •Added `.clearwelcome` disbale welcome message in your chat.
+  
     """
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
