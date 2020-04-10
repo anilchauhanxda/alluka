e1159390fc252b035cf02fe355682a9c3065f414
Anil chauhan <62683938+anilchauhanxda@users.noreply.github.com>
2020-04-10 09:41:52 +0000

    Update link_to_meanya.py

--- stdplugins/link_to_meanya.py
+++ stdplugins/link_to_meanya.py
@@ -1,7 +1,19 @@
 from telethon import events
+import os
+import requests
+import json
+from uniborg.util import admin_cmd
 
-@borg.on(events.NewMessage(chats=-1001128441021))
-async def handler(event):
-    
-    
-    	await borg.send_message("@meanyabot","/music " + event.message)
+@borg.on(events.NewMessage(pattern="https://?(.*)",))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = "/music " + event.pattern_match.group(1) 
+   
+    send = await borg.send_message("@meanyabot", "{}".format(input_str))
+       # error="undefined"
+        #if send = error
+        #await event.reply("Not able to Found 😕/n{} ".format(input_str))
+
+
+        
